2064b3c162946f94059ab73517ae82ef16f5a89a
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-05-13 00:45:14 +0200

    showing monthly scores

--- static/scripts/showScores.js
+++ static/scripts/showScores.js
@@ -34,3 +34,31 @@ allUserObjects.forEach((user) => {
 	`;
 	document.querySelector(".scoreList").insertAdjacentHTML("beforeend", userHtml);
 })
+
+const yourScoresContainer = document.querySelector(".yourScoreList");
+
+const today = new Date();
+const year = today.getFullYear().toString();
+userData.finishedChores.forEach(day => {
+	const dayYear = day.date.split("-")[0];
+	if (year === dayYear) {
+		const month = ((today.getMonth()+1) < 10 ? `0${today.getMonth()+1}` : today.getMonth()+1).toString();
+		const dayMonth = day.date.split("-")[1];
+		if (dayMonth === month) {
+			let thisMonthPoints = 0;
+			let monthsArray = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+			const thisMonth = monthsArray[today.getMonth()];
+			day.todaysChores.forEach(chore => thisMonthPoints = thisMonthPoints + chore.points);
+			const monthHtml = `
+			<div class="userBar">
+				<p>${thisMonth}</p>
+				<div class="barContainer" style="width: 100%;">
+					<div class="pointsBar" style="width: 100%;"></div>
+				</div>
+				<p class="userPoints">${thisMonthPoints}</p>
+			</div>
+			`;
+			yourScoresContainer.insertAdjacentHTML("beforeend", monthHtml);
+		}
+	}
+})
